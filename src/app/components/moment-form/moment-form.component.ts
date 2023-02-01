802e16f27113bbbeb26639cbf1fb639e43527244
William andrade <williamstrayder@gmail.com>
2023-02-01 08:37:20 -0300

    :sparkles: feat: Added validation with Reactive Forms

--- src/app/components/moment-form/moment-form.component.ts
+++ src/app/components/moment-form/moment-form.component.ts
@@ -1,10 +1,40 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
   templateUrl: './moment-form.component.html',
   styleUrls: ['./moment-form.component.css']
 })
-export class MomentFormComponent {
- @Input() btnText!: string;
+export class MomentFormComponent implements OnInit {
+  @Input() btnText!: string;
+
+  momentForm!: FormGroup;
+
+
+  ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(""),
+      title: new FormControl("", [Validators.required]),
+      description: new FormControl("", [Validators.required]),
+      image: new FormControl(""),
+    });
+  }
+
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+
+  get description(){
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+    console.log("chegou no submit")
+    if(this.momentForm.invalid){
+      return
+    }
+    console.log("submit")
+  }
 }
